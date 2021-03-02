9c36d56e89c1e023a0a3e998ba83669890dc67c7
Ashlesha Dule <77046834+Ashleshad@users.noreply.github.com>
2021-03-02 22:24:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+
+    var p = document.getElementsByTagName('p');
+    var scroll = document.getElementsByClassName('scroll');
+    var dragItem = null;
+
+
+
+
+    for (var i of p) {
+        i.addEventListener('dragstart', dragStart);
+        i.addEventListener('dragend', dragEnd);
+
+    }
+
+    function dragStart() {
+        dragItem = this
+        setTimeout(() => this.style.display = "none", 0);
+    }
+
+    function dragEnd() {
+        setTimeout(() => this.style.display = "block", 0);
+        dragItem = null;
+    }
+
+    for (j of scroll) {
+        j.addEventListener('dragover', dragOver);
+        j.addEventListener('dragenter', dragEnter);
+        j.addEventListener('dragleave', dragLeave);
+        j.addEventListener('drop', Drop);
+
+    }
+
+    function Drop() {
+        this.append(dragItem);
+    }
+
+    function dragOver(e) {
+        e.preventDefault();
+    }
+
+    function dragEnter(e) {
+        e.preventDefault();
+    }
+
+    function dragLeave(e) {
+        e.preventDefault();
+    }
+
+    
